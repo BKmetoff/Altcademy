85f7e8280500b396181560bee1f553e2ed0cc637
BKmetoff <bkmetoff@gmail.com>
2020-03-04 11:37:58 +0200

    Examples for ReactJSX & local compiling

---
+++ ReactJS/Exercises/02-BasicReact/03-Compile-JSX-Locally/script.js
@@ -0,0 +1,7 @@
+var element = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "hello world"
+);
+
+ReactDOM.render(element, document.getElementById('root'));
---
+++ ReactJS/Exercises/02-BasicReact/03-Compile-JSX-Locally/src/script.js
@@ -0,0 +1,6 @@
+const element = <h1 className="greeting">hello world</h1>
+
+ReactDOM.render (
+  element,
+  document.getElementById('root')
+);
