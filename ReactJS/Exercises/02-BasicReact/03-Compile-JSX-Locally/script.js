6d4fa2e37fbee0aa569365adb249f8a46ed09f19
BKmetoff <bkmetoff@gmail.com>
2020-03-05 10:23:56 +0200

    Example for a custom component

--- ReactJS/Exercises/02-BasicReact/03-Compile-JSX-Locally/script.js
+++ ReactJS/Exercises/02-BasicReact/03-Compile-JSX-Locally/script.js
@@ -1,7 +1,36 @@
 var element = React.createElement(
-  "h1",
-  { className: "greeting" },
-  "hello world"
+  'h1',
+  { className: 'greeting' },
+  'hello world'
 );
 
 ReactDOM.render(element, document.getElementById('root'));
+
+// custom components:
+var Temp = function Temp(props) {
+  console.log('props: ', props);
+  return React.createElement(
+    'p',
+    null,
+    'The current temperature in ',
+    props.city,
+    ' is ',
+    props.degree,
+    ' degree ',
+    props.unit
+  );
+};
+
+var App = function App() {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(Temp, { city: 'London', degree: 20, unit: 'celsius' }),
+    React.createElement(Temp, { city: 'NYC', degree: 23, unit: 'celsius' }),
+    React.createElement(Temp, { city: 'Dubai', degree: 32, unit: 'celsius' })
+  );
+};
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+
+// composing components:
