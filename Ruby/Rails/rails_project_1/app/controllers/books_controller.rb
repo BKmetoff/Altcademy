9082ec4c60d68f9b15c5cd4d08ba47845daa7a0f
BKmetoff <bkmetoff@gmail.com>
2020-06-22 11:37:04 +0200

    test: evaluating response body for Create Book

--- Ruby/Rails/rails_project_1/app/controllers/books_controller.rb
+++ Ruby/Rails/rails_project_1/app/controllers/books_controller.rb
@@ -4,12 +4,12 @@ class BooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    new_book = Book.create(
+    @book = Book.create(
       name: params[:name],
       publishing_year: params[:publishing_year],
       num_of_pages: params[:num_of_pages]
     )
 
-    render json: { book: new_book }
+    render 'books/create.jbuilder'
   end
 end
