f2976a70bdbfb370eddaa1de41aa4d69dd183027
BKmetoff <bkmetoff@gmail.com>
2020-07-06 15:26:34 +0200

    feat: add remaining job properties to controller

--- Ruby/Rails/rails_app_quiz/app/controllers/jobs_controller.rb
+++ Ruby/Rails/rails_app_quiz/app/controllers/jobs_controller.rb
@@ -10,9 +10,16 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.create(
-      job_title: params[:job_title],
+      url: params[:url],
       employer_name: params[:employer_name],
-      employer_description: params[:employer_description]
+      employer_description: params[:employer_description],
+      job_title: params[:job_title],
+      job_description: params[:job_description],
+      year_of_experience: params[:year_of_experience],
+      education_requirement: params[:education_requirement],
+      industry: params[:industry],
+      base_salary: params[:base_salary],
+      employment_type_id: params[:employment_type_id]
     )
     render 'jobs/create'
 
