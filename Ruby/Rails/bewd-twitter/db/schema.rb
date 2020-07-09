e48f26083b210d7d971e23f1d28e253ff4ce6e4b
BKmetoff <bkmetoff@gmail.com>
2020-07-09 13:09:47 +0200

    feat: add relationship tweets -> user

--- Ruby/Rails/bewd-twitter/db/schema.rb
+++ Ruby/Rails/bewd-twitter/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_091141) do
+ActiveRecord::Schema.define(version: 2020_07_09_091721) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "token"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_07_09_091141) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
