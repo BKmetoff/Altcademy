761bcf5abca23b57afd209173601265cb4f2b55e
BKmetoff <bkmetoff@gmail.com>
2020-07-09 18:49:01 +0200

    feat: create tweet action & route

--- Ruby/Rails/bewd-twitter/app/controllers/tweets_controller.rb
+++ Ruby/Rails/bewd-twitter/app/controllers/tweets_controller.rb
@@ -1,3 +1,34 @@
-class TweetsController < ApplicationController
+# frozen_string_literal: true
 
+class TweetsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+      @tweet = user.tweets.new(tweet_params)
+
+      if @tweet.save
+        render json: {
+          tweet: {
+            username: user.username,
+            message: @tweet.message
+          }
+        }
+      else
+        render json: { success: false }
+      end
+    else
+      render json: { session: false }
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message)
+  end
 end
