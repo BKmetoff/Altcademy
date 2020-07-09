a29ca48d030e4ff79560bbc0d37af6ba3da21487
BKmetoff <bkmetoff@gmail.com>
2020-07-09 11:01:19 +0200

    create sessions and sessions attr validations

---
+++ Ruby/Rails/bewd-twitter/db/migrate/20200709083802_create_sessions.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateSessions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sessions do |t|
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
--- Ruby/Rails/bewd-twitter/db/schema.rb
+++ Ruby/Rails/bewd-twitter/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_082701) do
+ActiveRecord::Schema.define(version: 2020_07_09_083802) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
