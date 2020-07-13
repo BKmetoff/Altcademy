2579431c45d934c77962687c61d80d8202fd6d66
BKmetoff <bkmetoff@gmail.com>
2020-07-13 12:33:57 +0200

    feat: config active_storage settings for Dev & Test env

--- Ruby/Rails/bewd-twitter-advanced/config/environments/development.rb
+++ Ruby/Rails/bewd-twitter-advanced/config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -51,4 +53,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Local settings for Active Storage
+  config.active_storage.service = :local
 end
