1ca325093790a9c6f97aca97653bd1f4a0c1935e
BKmetoff <bkmetoff@gmail.com>
2020-07-14 17:28:54 +0200

    fix: index newest tweets first

--- Ruby/Rails/bewd-twitter-advanced/app/controllers/tweets_controller.rb
+++ Ruby/Rails/bewd-twitter-advanced/app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@
 
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all.order(created_at: :desc)
+    @tweets = Tweet.all.order(created_at: :asc)
     render 'tweets/index'
   end
 
