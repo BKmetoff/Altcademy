cdcb6470a07ee93f22d358c32602356f2e65ddcb
BKmetoff <bkmetoff@gmail.com>
2020-08-31 11:30:47 +0200

    feat: model validations & relationships

--- FullStackDeployment/full-stack-twitter/app/models/session.rb
+++ FullStackDeployment/full-stack-twitter/app/models/session.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Session < ApplicationRecord
+  validates :user_id, presence: true
+  
+  belongs_to :user
+
+  #  generate token before validations
 end
--- FullStackDeployment/full-stack-twitter/app/models/tweet.rb
+++ FullStackDeployment/full-stack-twitter/app/models/tweet.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Tweet < ApplicationRecord
+  validates :user_id, presence: true
+  validates :message, presence: true, length: { minimum: 1, maximum: 140 }
+
+  belongs_to :user
 end
