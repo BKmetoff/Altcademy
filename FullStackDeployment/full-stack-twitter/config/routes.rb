abdff4f5ee5de5e22ab1544783a3a340ecb5f492
Boyan Kmetov <bkmetoff@gmail.com>
2020-09-04 09:42:12 +0200

    feat: user authentication api

--- FullStackDeployment/full-stack-twitter/config/routes.rb
+++ FullStackDeployment/full-stack-twitter/config/routes.rb
@@ -5,16 +5,11 @@ Rails.application.routes.draw do
   get 'demo' => 'static_pages#demo'
 
   post '/users' => 'users#create'
-
-  #  new session, set session token
-  #  compare input PW to DB PW
+ 
   post '/sessions' => 'sessions#create'
-
-  # retrieve cookie, find cookie in session table
   get '/authenticated' => 'sessions#authenticated'
-
-  # find cookie in sessions table, delete session if found
   delete '/sessions' => 'sessions#destroy'
+
 
   # find current user, create tweet
   post '/tweets' => 'tweets#create'
