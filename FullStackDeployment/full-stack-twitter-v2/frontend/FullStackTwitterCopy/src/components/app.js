fa79a9a6df76c28281fd8e6a3002b709041ee1e6
Boyan Kmetov <bkmetoff@gmail.com>
2020-09-13 11:37:36 +0200

    feat: (FE) chekc if user is logged in on load

--- FullStackDeployment/full-stack-twitter-v2/frontend/FullStackTwitterCopy/src/components/app.js
+++ FullStackDeployment/full-stack-twitter-v2/frontend/FullStackTwitterCopy/src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import Home from './Home'
 import Tweets from './Tweets'
@@ -14,6 +15,38 @@ export default class App extends Component {
 		}
 
 		this.handleLogin = this.handleLogin.bind(this)
+		this.checkLoginStatus = this.checkLoginStatus.bind(this)
+	}
+
+	componentDidMount() {
+		this.checkLoginStatus()
+	}
+
+	checkLoginStatus() {
+		axios
+			.get('http://localhost:3001/logged_in', { withCredentials: true })
+			.then((response) => {
+				if (
+					response.data.logged_in &&
+					this.state.loggedInStatus === 'NOT_LOGGED_IN'
+				) {
+					this.setState({
+						loggedInStatus: 'LOGGED_IN',
+						user: response.data.user,
+					})
+				} else if (
+					!response.data.logged_in &&
+					this.state.loggedInStatus === 'LOGGED_IN'
+				) {
+					this.setState({
+						loggedInStatus: 'NOT_LOGGED_IN',
+						user: {},
+					})
+				}
+			})
+			.catch((error) => {
+				console.log('log in error:', error)
+			})
 	}
 
 	handleLogin(userData) {
