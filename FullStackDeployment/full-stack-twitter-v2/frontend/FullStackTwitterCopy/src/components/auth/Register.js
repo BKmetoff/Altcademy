fdb8599f5f52dcf84af8a76de9fa8e5aaecb59f8
Boyan Kmetov <bkmetoff@gmail.com>
2020-09-13 10:38:50 +0200

    feat (FE) resitration react component

--- FullStackDeployment/full-stack-twitter-v2/frontend/FullStackTwitterCopy/src/components/auth/Register.js
+++ FullStackDeployment/full-stack-twitter-v2/frontend/FullStackTwitterCopy/src/components/auth/Register.js
@@ -1,7 +1,85 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Register extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			email: '',
+			password: '',
+			password_confirmation: '',
+			registrationErrors: '',
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleSubmit(event) {
+		const { email, password, password_confirmation } = this.state
+
+		axios
+			.post(
+				'http://localhost:3001/registrations',
+				{
+					user: {
+						email: email,
+						password: password,
+						password_confirmation: password_confirmation,
+					},
+				},
+				{
+					withCredentials: true,
+				}
+			)
+			.then((response) => {
+				console.log('reg response:', response)
+			})
+			.catch((error) => {
+				console.log('reg error', error)
+			})
+
+		event.preventDefault()
+	}
+
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		})
+	}
+
 	render() {
-		return <div>Register here!</div>
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type='email'
+						name='email'
+						placeholder='email'
+						value={this.state.email}
+						onChange={this.handleChange}
+						required
+					/>
+					<input
+						type='password'
+						name='password'
+						placeholder='password'
+						value={this.state.password}
+						onChange={this.handleChange}
+						required
+					/>
+					<input
+						type='password'
+						name='password_confirmation'
+						placeholder='password confirmation'
+						value={this.state.password_confirmation}
+						onChange={this.handleChange}
+						required
+					/>
+					<button type='submit'>register</button>
+				</form>
+			</div>
+		)
 	}
 }
