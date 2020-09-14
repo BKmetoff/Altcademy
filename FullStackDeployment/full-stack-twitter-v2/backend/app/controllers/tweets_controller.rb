9e08d98da1d67888a5ccdbd6aa77d26d0d36a98e
Boyan Kmetov <bkmetoff@gmail.com>
2020-09-14 11:35:02 +0200

    feat: (FE) delete tweet action. refactor craete action

--- FullStackDeployment/full-stack-twitter-v2/backend/app/controllers/tweets_controller.rb
+++ FullStackDeployment/full-stack-twitter-v2/backend/app/controllers/tweets_controller.rb
@@ -1,19 +1,39 @@
 class TweetsController < ApplicationController
   include CurrentUserConcern
 
-  def create
-    if @current_user
-      tweet = Tweet.create!(
-        message: params['message'],
-        user_id: @current_user.id
-      )
+  before_action :validate_user
 
+  def create
+    tweet = Tweet.create!(
+      message: params['message'],
+      user_id: @current_user.id
+    )
+
+    if tweet
       render json: {
         status: :created,
         tweet: tweet
       }
     else
-      render json: { status: 401 }
+      render json: { status: 500 }
     end
+  end
+
+  def destroy
+    tweet = Tweet.find_by(id: params[:id])
+
+    return render json: { status: 401 } unless tweet.user_id == @current_user.id
+
+    if tweet.destroy
+      render json: { status: :deleted }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  private
+
+  def validate_user
+    return render json: { status: 401 } unless @current_user
   end
 end
