73a3d7342ed8e48a77ad9febd900ec8e4d82fcc3
Boyan Kmetov <bkmetoff@gmail.com>
2020-09-14 17:07:46 +0200

    feat: (FE) create new tweet & update component state

--- FullStackDeployment/full-stack-twitter-v2/frontend/FullStackTwitterCopy/src/components/Tweets.js
+++ FullStackDeployment/full-stack-twitter-v2/frontend/FullStackTwitterCopy/src/components/Tweets.js
@@ -7,7 +7,7 @@ const _ = require('lodash')
 const Tweets = (props) => {
 	let history = useHistory()
 
-	const [tweets, setTweets] = useState()
+	const [state, setState] = useState({ tweets: [], newTweet: '' })
 
 	const handleLogoutClick = () => {
 		axios
@@ -21,22 +21,43 @@ const Tweets = (props) => {
 			})
 	}
 
-	function getTweets() {
+	useEffect(() => {
+		getTweets()
+	}, [])
+
+	const getTweets = () => {
 		axios
 			.get('http://localhost:3001/tweets', { withCredentials: true })
 			.then((response) => {
-				setTweets({
-					tweets: response.data.tweets,
-				})
+				setState({ ...state, tweets: response.data.tweets })
 			})
 			.catch((error) => {
 				console.log('get tweets error: ', error)
 			})
 	}
 
-	useEffect(() => {
-		getTweets()
-	}, [])
+	const postTweet = (e) => {
+		e.preventDefault()
+		axios
+			.post(
+				'http://localhost:3001/tweets',
+				{ message: state.newTweet },
+				{ withCredentials: true }
+			)
+			.then((response) => {
+				setState({ ...state, newTweet: '' })
+				getTweets()
+				console.log('new tweet response:', response.data)
+			})
+			.catch((error) => {
+				console.log('new tweet error:', error)
+			})
+	}
+
+	const handleChange = (e) => {
+		e.preventDefault()
+		setState({ ...state, newTweet: e.target.value })
+	}
 
 	return (
 		<div>
@@ -53,9 +74,28 @@ const Tweets = (props) => {
 				<div>
 					<h1>tweets</h1>
 					<button onClick={handleLogoutClick}>Log out</button>
+					{/* {console.log(props)} */}
+					<form onSubmit={postTweet}>
+						<input
+							type='text'
+							placeholder='sup?'
+							name='tweet'
+							value={state.newTweet}
+							onChange={handleChange}
+							required
+						/>
+						<button type='submit'>post</button>
+					</form>
 					<ul>
-						{_.flatMap(tweets).map((tweet) => {
-							return <li key={tweet.id}>{tweet.message}</li>
+						{_.flatMap(state.tweets).map((tweet) => {
+							return (
+								<li key={tweet.id}>
+									<div>
+										<button>delete</button>
+										{tweet.message}
+									</div>
+								</li>
+							)
 						})}
 					</ul>
 				</div>
