8ca9003cd6b2be49608a28ebafde3eb1ac32be2b
Boyan Kmetov <bkmetoff@gmail.com>
2020-12-21 14:00:05 +0100

    finish mock-up app

--- Capstone-1/src/App.js
+++ Capstone-1/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react'
+import * as mobilenet from '@tensorflow-models/mobilenet'
+import * as tf from '@tensorflow/tfjs'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// requiored for TensorFlow to start
+	tf.setBackend('cpu')
+
+	const [imgUrl, setImgUrl] = useState()
+	const [showImg, setShowImg] = useState(false)
+	const [model, setModel] = useState(null)
+	const [predictionResults, setPredictionResults] = useState([])
+	const [isGuitar, setIsGuitar] = useState(null)
+
+	const imageRef = useRef()
+	const inputRef = useRef()
+
+	useEffect(() => {
+		loadModel()
+	}, [])
+
+	const loadModel = async () => {
+		const mobilenetModel = await mobilenet.load()
+		setModel(mobilenetModel)
+	}
+
+	const handleReset = (e) => {
+		setShowImg(false)
+		setImgUrl('')
+		setPredictionResults([])
+		setIsGuitar(null)
+	}
+
+	const handleUpload = (e) => {
+		const { files } = e.target
+		if (files.length > 0) {
+			const url = URL.createObjectURL(files[0])
+			setImgUrl(url)
+			setShowImg(true)
+		}
+	}
+
+	const uploadImg = () => inputRef.current.click()
+
+	const identify = async () => {
+		const results = await model.classify(imageRef.current)
+		setPredictionResults(results)
+
+		const isGuitarCheck = (resultsElement) => {
+			return resultsElement.className.includes('guitar')
+		}
+
+		setIsGuitar(results.some(isGuitarCheck))
+	}
+
+	if (!model) {
+		return (
+			<div className='App'>
+				<p>loading magic...</p>
+			</div>
+		)
+	}
+
+	return (
+		<div className='App'>
+			{showImg && (
+				<img
+					crossOrigin='anonymous'
+					alt='upload-preview'
+					src={imgUrl}
+					ref={imageRef}
+				/>
+			)}
+			<input
+				type='file'
+				accept='image/*'
+				capture='camera'
+				onChange={handleUpload}
+				ref={inputRef}
+				hidden
+			/>
+
+			{predictionResults.length !== 0 ? (
+				<div className='result-message'>
+					<h3>{isGuitar ? "it's a guitar." : "it's not a guitar. "}</h3>
+				</div>
+			) : (
+				<button onClick={!imgUrl ? uploadImg : identify}>
+					{!imgUrl ? 'upload image' : 'identify'}
+				</button>
+			)}
+
+			{imgUrl && <button onClick={handleReset}>reset</button>}
+		</div>
+	)
 }
 
-export default App;
+export default App
