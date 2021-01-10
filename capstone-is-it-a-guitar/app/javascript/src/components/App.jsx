100027b92099f8693497422093e640da9c3f3bc9
Boyan Kmetov <bkmetoff@gmail.com>
2021-01-10 16:18:37 +0100

    feat: finish checkGuitar basic version

--- capstone-is-it-a-guitar/app/javascript/src/components/App.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/App.jsx
@@ -1,6 +1,9 @@
-import React, { useReducer } from 'react'
+import React, { useState, useReducer, useRef } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
+
+import * as mobilenet from '@tensorflow-models/mobilenet'
+import * as tf from '@tensorflow/tfjs'
 
 import Layout from './Layout'
 import History from './History'
@@ -9,15 +12,27 @@ import CheckGuitar from './CheckGuitar'
 import LoginSignUp from './LoginSignUp'
 import NotFound from './NotFound'
 
+const routes = [
+	{ component: History, path: '/history' },
+	{ component: Leaderboard, path: '/leaderboard' },
+	{ component: LoginSignUp, path: '/login' },
+	{ component: NotFound, path: '/' },
+]
+
+// state below is global
 const stateMachine = {
 	initial: 'initial',
 	states: {
 		initial: { on: { next: 'loadingModel' } },
 		loadingModel: { on: { next: 'awaitingUpload' } },
 		awaitingUpload: { on: { next: 'ready' } },
-		ready: { on: { next: 'classifying' } },
+		ready: { on: { next: 'classifying' }, showImage: true },
 		classifying: { on: { next: 'complete' } },
-		complete: { on: { next: 'awaitingUpload' } },
+		complete: {
+			on: { next: 'awaitingUpload' },
+			showImage: true,
+			showResults: true,
+		},
 	},
 }
 
@@ -26,28 +41,33 @@ const reducer = (currentState, event) => {
 }
 
 export default function App() {
+	tf.setBackend('cpu')
 	const [state, dispatch] = useReducer(reducer, stateMachine.initial)
+	const [model, setModel] = useState(null)
 
-	const routes = [
-		// { component: CheckGuitar, path: '/', exact: true, dispatch: { dispatch } },
-		{ component: History, path: '/history' },
-		{ component: Leaderboard, path: '/leaderboard' },
-		{ component: LoginSignUp, path: '/login' },
-		{ component: NotFound, path: '/' },
-	]
+	const next = () => dispatch('next')
+
+	const loadModel = async () => {
+		next()
+		const mobilenetModel = await mobilenet.load()
+		setModel(mobilenetModel)
+		next()
+	}
 
 	return (
 		<Router>
 			<Layout>
 				<Switch>
 					<Route path='/' exact>
-						<CheckGuitar dispatch={dispatch} state={state} />
+						<CheckGuitar
+							state={state}
+							stateMachine={stateMachine}
+							nextState={next}
+							loadModel={loadModel}
+							model={model}
+						/>
 					</Route>
 					{routes.map((props) => {
-						// props.component === CheckGuitar ?
-						// return <Route>
-						// <CheckGuitar pa
-						// </Route>:
 						return <Route key={props.path} {...props} />
 					})}
 				</Switch>
--- capstone-is-it-a-guitar/app/javascript/src/components/CheckGuitar.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/CheckGuitar.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-export default function CheckGuitar({ dispatch, state }) {
+export default function CheckGuitar({
+	state,
+	nextState,
+	stateMachine,
+	loadModel,
+	model,
+}) {
+	const [imageURL, setImageURL] = useState(null)
+	const [isGuitar, setIsGuitar] = useState(false)
+	const [predictionResults, setPredictionResults] = useState([])
+	const imageRef = useRef()
+	const inputRef = useRef()
+
+	const { showImage = false, showResults = false } = stateMachine.states[state]
+
+	const buttonProps = {
+		initial: { text: 'Load', action: loadModel },
+		loadingModel: { text: 'Loading Magic...', action: () => {} },
+		awaitingUpload: {
+			text: 'Loaded. Upload photo',
+			action: () => inputRef.current.click(),
+		},
+		ready: { text: 'Go!', action: () => identify() },
+		classifying: { text: 'Working...', action: () => {} },
+		complete: { text: 'Reset!', action: () => reset() },
+	}
+
+	const handleUpload = (e) => {
+		const { files } = e.target
+		if (files.length > 0) {
+			const url = URL.createObjectURL(files[0])
+			setImageURL(url)
+			nextState()
+		}
+	}
+
+	const identify = async () => {
+		nextState()
+
+		const results = await model.classify(imageRef.current)
+		setPredictionResults(results)
+
+		const isGuitarCheck = (resultsElement) => {
+			return resultsElement.className.includes('guitar')
+		}
+
+		setIsGuitar(results.some(isGuitarCheck))
+		nextState()
+	}
+
+	const reset = () => {
+		setPredictionResults([])
+		setImageURL(null)
+		nextState()
+	}
+
 	return (
-		<div>
-			this is check guitar
-			<button onClick={() => dispatch('next')}>{state}</button>
+		<div
+			style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+		>
+			{console.log(state, isGuitar)}
+
+			<input
+				type='file'
+				accept='image/*'
+				capture='camera'
+				ref={inputRef}
+				onChange={handleUpload}
+				hidden
+			/>
+			<button onClick={buttonProps[state].action}>
+				{buttonProps[state].text}
+			</button>
+			{showResults && (
+				<div>{isGuitar ? 'It is a guitar.' : 'It is not a guitar'}</div>
+			)}
+			{showImage && <img alt='upload-preview' src={imageURL} ref={imageRef} />}
 		</div>
 	)
 }
