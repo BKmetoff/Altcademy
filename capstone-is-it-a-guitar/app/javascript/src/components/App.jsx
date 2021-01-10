546ebaf10ba24b6ef5ea618797d0b259e1093686
Boyan Kmetov <bkmetoff@gmail.com>
2021-01-10 13:05:24 +0100

    feat: initial setup of reducer & stateMachine

--- capstone-is-it-a-guitar/app/javascript/src/components/App.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 
@@ -8,18 +8,40 @@ import Leaderboard from './Leaderboard'
 import CheckGuitar from './CheckGuitar'
 import LoginSignUp from './LoginSignUp'
 
-const routes = [
-	{ component: CheckGuitar, path: '/', exact: true },
-	{ component: History, path: '/history' },
-	{ component: Leaderboard, path: '/leaderboard' },
-	{ component: LoginSignUp, path: '/login' },
-]
+const stateMachine = {
+	initial: 'initial',
+	states: {
+		initial: { on: { next: 'loadingModel' } },
+		loadingModel: { on: { next: 'awaitingUpload' } },
+		awaitingUpload: { on: { next: 'ready' } },
+		ready: { on: { next: 'classifying' } },
+		classifying: { on: { next: 'complete' } },
+		complete: { on: { next: 'awaitingUpload' } },
+	},
+}
+
+const reducer = (currentState, event) => {
+	return stateMachine.states[currentState].on[event] || stateMachine.initial
+}
 
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, stateMachine.initial)
+
+	const routes = [
+		// { component: CheckGuitar, path: '/', exact: true, dispatch: { dispatch } },
+		{ component: History, path: '/history' },
+		{ component: Leaderboard, path: '/leaderboard' },
+		{ component: LoginSignUp, path: '/login' },
+	]
+
 	return (
 		<Router>
 			<Layout>
 				<Switch>
+					<Route to='/' exact>
+						<CheckGuitar dispatch={dispatch} state={state} />
+					</Route>
+
 					{routes.map((props) => {
 						return <Route key={props.path} {...props} />
 					})}
--- capstone-is-it-a-guitar/app/javascript/src/components/CheckGuitar.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/CheckGuitar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export default function CheckGuitar() {
-	return <div>this is check guitar</div>
+export default function CheckGuitar({ dispatch, state }) {
+	return (
+		<div>
+			this is check guitar
+			<button onClick={() => dispatch('next')}>{state}</button>
+		</div>
+	)
 }
