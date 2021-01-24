02833e2f56cffdbe2655a863a128ef10cb2c7e79
Boyan Kmetov <bkmetoff@gmail.com>
2021-01-24 10:40:05 +0200

    feat: add global styles & theme provider

--- capstone-is-it-a-guitar/app/javascript/src/components/Layout.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/Layout.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 import Header from './Header'
 import { Container } from '../backbone/Container'
 
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle, Theme } from '../backbone/style/Theme'
+
 export default function Layout({ children }) {
 	return (
-		<Container>
-			<Header />
-			{children}
-		</Container>
+		<ThemeProvider theme={Theme}>
+			<GlobalStyle />
+			<Container>
+				<Header />
+				{children}
+			</Container>
+		</ThemeProvider>
 	)
 }
