798d74a22b5c738114a1781b089061af0fed1834
Boyan Kmetov <bkmetoff@gmail.com>
2021-02-26 14:13:04 +0100

    feat: (be) create user model, controller, migration

---
+++ capstone-is-it-a-guitar/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render 'api/users/create'
+    else
+      render json: { success: false }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
+end
