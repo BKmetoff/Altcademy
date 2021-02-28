e5b724a367c77be3a2883cbcad7a7777e5756c7e
Boyan Kmetov <bkmetoff@gmail.com>
2021-02-28 12:45:23 +0100

    feat: (be) add relationship user <-> session

--- capstone-is-it-a-guitar/app/models/user.rb
+++ capstone-is-it-a-guitar/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :attempts
+  has_many :sessions
 
   validates :username, presence: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 3, maximum: 64 }
