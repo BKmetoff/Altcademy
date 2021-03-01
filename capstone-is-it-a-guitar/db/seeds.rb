cedb0f22f9c86fa4e6d46839a2c5ffa51f70931d
Boyan Kmetov <bkmetoff@gmail.com>
2021-03-01 10:20:36 +0100

    feat: (be) seed data

--- capstone-is-it-a-guitar/db/seeds.rb
+++ capstone-is-it-a-guitar/db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([
+                      { username: 'Tommy', email: 'tommy@test.com', password: 'password' },
+                      { username: 'Bobby', email: 'bobby@test.com', password: 'password' },
+                      { username: 'Sarah', email: 'sarah@test.com', password: 'password' },
+                      { username: 'Lilly', email: 'lilly@test.com', password: 'password' },
+                      { username: 'Jimmy', email: 'jimmy@test.com', password: 'password' },
+                      { username: 'Cammy', email: 'cammy@test.com', password: 'password' }
+                    ])
+
+attempts = Attempt.create([
+                            { user_id: 1, image_url: 'url-here', success: true },
+                            { user_id: 1, image_url: 'url-here', success: true },
+                            { user_id: 1, image_url: 'url-here', success: true },
+                            { user_id: 1, image_url: 'url-here', success: true },
+                            { user_id: 2, image_url: 'url-here', success: false },
+                            { user_id: 2, image_url: 'url-here', success: true },
+                            { user_id: 2, image_url: 'url-here', success: true },
+                            { user_id: 2, image_url: 'url-here', success: false },
+                            { user_id: 2, image_url: 'url-here', success: false },
+                            { user_id: 2, image_url: 'url-here', success: false },
+                            { user_id: 2, image_url: 'url-here', success: false },
+                            { user_id: 2, image_url: 'url-here', success: false },
+                            { user_id: 3, image_url: 'url-here', success: false },
+                            { user_id: 3, image_url: 'url-here', success: false },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: true },
+                            { user_id: 3, image_url: 'url-here', success: false },
+                            { user_id: 4, image_url: 'url-here', success: false },
+                            { user_id: 4, image_url: 'url-here', success: false },
+                            { user_id: 4, image_url: 'url-here', success: true },
+                            { user_id: 4, image_url: 'url-here', success: true },
+                            { user_id: 4, image_url: 'url-here', success: false },
+                            { user_id: 5, image_url: 'url-here', success: true },
+                            { user_id: 5, image_url: 'url-here', success: true },
+                            { user_id: 5, image_url: 'url-here', success: true },
+                            { user_id: 5, image_url: 'url-here', success: true },
+                            { user_id: 5, image_url: 'url-here', success: false },
+                            { user_id: 6, image_url: 'url-here', success: false },
+                            { user_id: 6, image_url: 'url-here', success: false },
+                            { user_id: 6, image_url: 'url-here', success: false },
+                            { user_id: 6, image_url: 'url-here', success: false },
+                            { user_id: 6, image_url: 'url-here', success: true },
+                            { user_id: 6, image_url: 'url-here', success: true },
+                            { user_id: 6, image_url: 'url-here', success: true },
+                            { user_id: 6, image_url: 'url-here', success: true },
+                            { user_id: 6, image_url: 'url-here', success: true },
+                            { user_id: 7, image_url: 'url-here', success: true },
+                            { user_id: 7, image_url: 'url-here', success: false },
+                            { user_id: 7, image_url: 'url-here', success: false },
+                            { user_id: 7, image_url: 'url-here', success: true },
+                            { user_id: 8, image_url: 'url-here', success: true },
+                            { user_id: 8, image_url: 'url-here', success: true },
+                            { user_id: 8, image_url: 'url-here', success: true },
+                            { user_id: 8, image_url: 'url-here', success: true },
+                            { user_id: 8, image_url: 'url-here', success: true },
+                            { user_id: 8, image_url: 'url-here', success: false},
+                            { user_id: 8, image_url: 'url-here', success: false }
+                          ])
