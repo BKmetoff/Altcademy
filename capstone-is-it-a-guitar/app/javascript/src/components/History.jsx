ba449cd5c73568d7b5d07682afee7604a3301bb3
Boyan Kmetov <bkmetoff@gmail.com>
2021-03-03 12:22:19 +0100

    feat: (fe) use current user context and LoginError on History component

--- capstone-is-it-a-guitar/app/javascript/src/components/History.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/History.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CurrentUserContext } from '../components/App'
+
+import LogInError from './LogInError'
 import ImageCard from '../backbone/ImageCard'
 
 import { MOCK_DATA } from '../utils/mock'
 
 export default function History() {
+	const { userLoggedInStatus } = useContext(CurrentUserContext)
+
+	if (!userLoggedInStatus.loggedIn && !userLoggedInStatus.user.user_id) {
+		return <LogInError />
+	}
+
 	return (
 		<div>
 			<ImageCard image={MOCK_DATA.IMAGE} />
