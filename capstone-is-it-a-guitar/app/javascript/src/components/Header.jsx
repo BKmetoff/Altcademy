0e039d29921c7eb006e123aa3aecc676eba53106
Boyan Kmetov <bkmetoff@gmail.com>
2021-03-03 17:19:10 +0100

    feat: (fe) consume api/sessions/destroy in Header component

--- capstone-is-it-a-guitar/app/javascript/src/components/Header.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/Header.jsx
@@ -1,19 +1,47 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { CurrentUserContext } from '../components/App'
+
+import { safeCredentials, handleErrors } from '../utils/fetchHelper'
 
 import Button from '../backbone/Button'
 import Divider from '../backbone/Divider'
 import { Wrapper } from '../backbone/Container'
 
 export default function Header() {
-	const { userLoggedInStatus } = useContext(CurrentUserContext)
+	let history = useHistory()
+	const { userLoggedInStatus, checkLoggedIn } = useContext(CurrentUserContext)
+
+	const logOutUser = () => {
+		fetch(
+			`/api/sessions/${userLoggedInStatus.user.user_id}`,
+			safeCredentials({
+				method: 'DELETE',
+			})
+		)
+			.then(handleErrors)
+			.then((data) => {
+				console.log('logout response data: ', data)
+				data.success && checkLoggedIn()
+			})
+			.then(history.push('/login'))
+			.then(window.location.reload())
+			.catch((error) => console.log('logout error: ', error))
+	}
 
 	return (
 		<Wrapper header column fullWidth>
 			<Wrapper justifyBetween>
-				<p>{userLoggedInStatus.user.username}</p>
-				<Button kind='secondary'>Log out</Button>
+				{userLoggedInStatus.loggedIn && (
+					<>
+						<p style={{ fontSize: 16, alignSelf: 'center' }}>
+							{userLoggedInStatus.user.username}
+						</p>
+						<Button kind='secondary' onClick={logOutUser}>
+							Log out
+						</Button>
+					</>
+				)}
 			</Wrapper>
 			<Wrapper justifyCenter>
 				<Button kind='headerLink'>
