53917aeb24054101c28efba4108e8727bb700bab
Boyan Kmetov <bkmetoff@gmail.com>
2021-03-05 15:51:24 +0100

    feat: (fe) styling, Footer

--- capstone-is-it-a-guitar/app/javascript/src/components/Footer.jsx
+++ capstone-is-it-a-guitar/app/javascript/src/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
 import { Wrapper } from '../backbone/Container'
 
 export default function Footer() {
 	return (
 		<Wrapper footer justifyBetween fullWidth>
-			<span>altcademy</span> <span>git</span>
+			<span>
+				<a href='https://www.altcademy.com/' target='_blank'>
+					altcademy
+				</a>
+			</span>
+
+			<span>
+				<a href='https://github.com/BKmetoff' target='_blank'>
+					git/bkmetoff
+				</a>
+			</span>
 		</Wrapper>
 	)
 }
